97966dcd55e737df788851868b3224c49ce99ca3
tkleinke <thomas.kleinke@gbv.de>
2025-01-31 11:56:05 +0100

    Check for conceptURI instead of conceptName in data endpoint

--- src/server/extension/data.js
+++ src/server/extension/data.js
@@ -4,6 +4,9 @@ const info = process.argv.length >= 3
 
 let input = '';
 
+const EVENT_TYPE_CONCEPT_URI = 'http://uri.gbv.de/terminology/object_related_event/978eb685-12d0-45d2-ac64-77bc64b7de0b';
+const IDENTIFIER_TYPE_CONCEPT_URI = 'http://uri.gbv.de/terminology/nld_identifier_type/39f08855-3d88-4435-889d-65bb63f042e5';
+
 process.stdin.on('data', d => {
     try {
         input += d.toString();
@@ -76,11 +79,11 @@ function getData(object, yesId) {
         title: object[objectType][nestedPrefix + 'titel']?.map(entry => entry.titel).join(', '),
         itemType: object[objectType].lk_objekttyp?.conceptName,
         itemIdentifier: object[objectType][nestedPrefix + 'identifier']
-            ?.find(entry => entry.lk_identifier_typ?.conceptName === 'ADABweb-Kennziffer')?.identifier,
+            ?.find(entry => entry.lk_identifier_typ?.conceptURI === IDENTIFIER_TYPE_CONCEPT_URI)?.identifier,
         detailViewUrl: info.external_url + '/#/detail/' + object._uuid,
         designationStatus: object[objectType][nestedPrefix + 'event']
             ?.find(entry => {
-                return entry.lk_eventtyp?.conceptName === 'Ausweisung'
+                return entry.lk_eventtyp?.conceptURI === EVENT_TYPE_CONCEPT_URI
                     && entry.lk_veroeffentlichen?.ja_nein_objekttyp._id === yesId;
             })?.lk_status?.conceptName,
         dorisDocuments: object[objectType][nestedPrefix + 'e_akte']
